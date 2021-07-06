3f0bac261cd9abdd225a0eb23b9e776b2a21c8fd
naram.dash <juho_kim@outlook.com>
2021-07-06 10:58:45 +0900

    reinit Azure Functions

--- SendMe.Api/KeysGet.cs
+++ SendMe.Api/KeysGet.cs
@@ -14,7 +14,7 @@ namespace SendMe.Api
     {
         [FunctionName("KeysGet")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = "keys")] HttpRequest req,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
