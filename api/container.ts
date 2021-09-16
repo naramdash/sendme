a6b9f5a306ccab4992b8d008d901e903083bb0f8
naram.dash <juho_kim@outlook.com>
2021-09-17 00:08:55 +0900

    sendme api basic works

---
+++ api/container.ts
@@ -0,0 +1,16 @@
+import { CosmosClient } from "@azure/cosmos";
+
+function getLinkContainer() {
+  const CONNECTION_STRING = process.env.CONNECTION_STRING;
+  const client = new CosmosClient(CONNECTION_STRING);
+
+  const DATABASE_STRING = "sendme";
+  const database = client.database(DATABASE_STRING);
+
+  const CONTAINER_STRING = "link";
+  const container = database.container(CONTAINER_STRING);
+
+  return container;
+}
+
+export { getLinkContainer };
